189fb9663d414dff3905594452957a570d1774da
KimJinsu66 <jinsu6688@gmail.com>
2021-02-15 00:36:33 +0900

    비교 연산자 내용입니다.

---
+++ javascript/2_javascripte_basic/2_9_comparsion_operator.js
@@ -0,0 +1,10 @@
+// 일치 연산자
+// 厳密等価演算子(げんみつとうかえんざんし), 一致
+
+/*
+  일차 연산자(strict equality operator) === 를 사용하면 
+  형변환 없이 값을 비교할 수 있다.
+*/
+
+console.log( 0 ==  false ); // true
+console.log( 0 === false ); // false
