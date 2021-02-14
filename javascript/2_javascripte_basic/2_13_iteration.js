ba11717b8892f3bb7a08111767443a09ca7f907a
KimJinsu66 <jinsu6688@gmail.com>
2021-02-15 00:40:26 +0900

    반복문 정리입니다

---
+++ javascript/2_javascripte_basic/2_13_iteration.js
@@ -0,0 +1,16 @@
+const admin = true;
+// while
+
+while (admin) {
+  console.log("무한 loop");
+}
+
+// for 기본 형
+for (let i = 0; i < 5; i ++) {
+  console.log(i);
+}
+
+for (;;) {
+  console.log("무한 loop");
+  break;
+}
