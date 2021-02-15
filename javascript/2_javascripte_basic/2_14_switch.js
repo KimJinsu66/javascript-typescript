34fbe333d642109b40b66cc4c0413b6669e6e43c
KimJinsu66 <jinsu6688@gmail.com>
2021-02-16 00:17:35 +0900

    switchまとめです

---
+++ javascript/2_javascripte_basic/2_14_switch.js
@@ -0,0 +1,17 @@
+// switch 문
+
+let fruit = "Apple";
+
+switch(fruit) {
+  case "Banana":
+    console.log("banana");
+    break;
+  case "Apple":
+    console.log("Apple");
+    break;
+  case "Melon":
+  case "Mango":
+    console.log("Melon & Mango");
+  default:
+    console.log("과일을 다시 입력하세요^^");
+}
