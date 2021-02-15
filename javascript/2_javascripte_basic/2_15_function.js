3214870c12084e50a643fe0b5d99525a22215055
KimJinsu66 <jinsu6688@gmail.com>
2021-02-16 00:17:51 +0900

    function정리입니다

---
+++ javascript/2_javascripte_basic/2_15_function.js
@@ -0,0 +1,17 @@
+//function(메소드)
+function showMessage(message) {
+    console.log(message);
+}
+
+message = "출력하자!";
+showMessage(message);
+
+// 표현식 2 변수에 저장
+let sayHi = function() {
+  return "Hi";
+};
+
+console.log(sayHi());
+
+// https://ko.javascript.info/function-expressions
+// 여기부터 공부하기
