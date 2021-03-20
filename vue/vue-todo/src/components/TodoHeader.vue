d8eab368c4126781a4af067cc9fde3c4a36a250e
KimJinsu66 <jinsu6688@gmail.com>
2021-03-20 20:52:15 +0900

    Todo List Header 입니다

---
+++ vue/vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>TODO it!</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+/* scope의 의미는 스타일 정의를 해당 컴포넌트에만 적용하겠다는 의미이다. */
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
